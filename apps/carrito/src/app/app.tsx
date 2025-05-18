488fa0c8786b8017b629d3f3a672704d7eb4d6c2
mariollori <mario.llori.20@gmail.com>
2025-05-18 17:54:09 -0500

    feat:Configuration Base

---
+++ apps/carrito/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  console.log('sexoooooooo')
+  return (
+    <div>
+      <NxWelcome title="carrito" />
+      <h1>sexooooooooooo</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/productos/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="productos" />
+    </div>
+  );
+}
+
+export default App;
